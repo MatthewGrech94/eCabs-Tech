9fa3b7516104c8628a093e9f8c3b3b8e1d0d2e75
Matthew Grech <matthew-grech@outlook.com>
2025-07-28 22:00:29 +0200

    feat: add testimonials CPT

--- app/public/wp-content/themes/ecabs-theme/functions.php
+++ app/public/wp-content/themes/ecabs-theme/functions.php
@@ -9,19 +9,81 @@ add_action('after_setup_theme', 'ecabs_register_menus');
 
 function ecabs_theme_enqueues() {
 
-
+    // Enqueue google font
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap', false );
+    wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css', array(), '6.5.0' );
 
     // Enqueue main stylesheet
     wp_enqueue_style( 'ecabs-theme-style', get_stylesheet_uri() );
 
     // Enqueue scripts
-    wp_enqueue_script(
-        'mytheme-scripts', 
-        get_template_directory_uri() . '/js/ecabs-theme-script.js', 
-        array('jquery'), 
-        '1.0.0',
-        true
-    );
+    wp_enqueue_script( 'ecabs-theme-script', get_template_directory_uri() . '/js/ecabs-theme-script.js', array('jquery'), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'ecabs_theme_enqueues' );
+
+function product_template_enqueues() {
+    if (is_page_template('product-template.php')) {
+        wp_enqueue_style('product-template-style', get_template_directory_uri() . '/css/custom-style.css');
+
+        // Enqueue scripts
+        wp_enqueue_script( 'product-template-script', get_template_directory_uri() . '/js/templates/product-template-script.js', array('jquery'), '1.0.0', true );
+    }
+}
+add_action('wp_enqueue_scripts', 'product_template_enqueues');
+
+function create_testimonials_custom_post_type() {
+
+    $labels = array(
+        'name'                  => _x('Testimonials', 'Post Type General Name', 'textdomain'),
+        'singular_name'         => _x('Testimonial', 'Post Type Singular Name', 'textdomain'),
+        'menu_name'             => __('Testimonials', 'textdomain'),
+        'name_admin_bar'        => __('Testimonial', 'textdomain'),
+        'archives'              => __('Testimonial Archives', 'textdomain'),
+        'attributes'            => __('Testimonial Attributes', 'textdomain'),
+        'parent_item_colon'     => __('Parent Testimonial:', 'textdomain'),
+        'all_items'             => __('All Testimonials', 'textdomain'),
+        'add_new_item'          => __('Add New Testimonial', 'textdomain'),
+        'add_new'               => __('Add New', 'textdomain'),
+        'new_item'              => __('New Testimonial', 'textdomain'),
+        'edit_item'             => __('Edit Testimonial', 'textdomain'),
+        'update_item'           => __('Update Testimonial', 'textdomain'),
+        'view_item'             => __('View Testimonial', 'textdomain'),
+        'view_items'            => __('View Testimonials', 'textdomain'),
+        'search_items'          => __('Search Testimonial', 'textdomain'),
+        'not_found'             => __('Not found', 'textdomain'),
+        'not_found_in_trash'    => __('Not found in Trash', 'textdomain'),
+        'featured_image'        => __('Client Image', 'textdomain'),
+        'set_featured_image'    => __('Set client image', 'textdomain'),
+        'remove_featured_image' => __('Remove client image', 'textdomain'),
+        'use_featured_image'    => __('Use as client image', 'textdomain'),
+        'insert_into_item'      => __('Insert into testimonial', 'textdomain'),
+        'uploaded_to_this_item' => __('Uploaded to this testimonial', 'textdomain'),
+        'items_list'            => __('Testimonials list', 'textdomain'),
+        'items_list_navigation' => __('Testimonials list navigation', 'textdomain'),
+        'filter_items_list'     => __('Filter testimonials list', 'textdomain'),
+    );
+
+    $args = array(
+        'label'                 => __('Testimonials', 'textdomain'),
+        'description'           => __('Client testimonials or reviews', 'textdomain'),
+        'labels'                => $labels,
+        'supports'              => array('title', 'editor', 'thumbnail'),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 20,
+        'menu_icon'             => 'dashicons-testimonial',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => false,
+        'can_export'            => true,
+        'has_archive'           => false,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'show_in_rest'          => true,
+        'capability_type'       => 'post',
+    );
+
+    register_post_type('testimonials', $args);
+}
+add_action('init', 'create_testimonials_custom_post_type', 0);

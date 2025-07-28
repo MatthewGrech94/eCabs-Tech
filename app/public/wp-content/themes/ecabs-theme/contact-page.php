ba899477354b594a5979072640016be422fcf2f0
Matthew Grech <matthew-grech@outlook.com>
2025-07-28 21:59:38 +0200

    feat: add contact page and styles

--- app/public/wp-content/themes/ecabs-theme/contact-page.php
+++ app/public/wp-content/themes/ecabs-theme/contact-page.php
@@ -8,18 +8,28 @@ Template Name: Contact Template
 get_header(); 
 ?>
 
-<div class="contact-template">
+<div class="contact-page">
 
-  <!-- Optional: WordPress content -->
   <div class="fold-default">
-    <div class="wrapper">
-    <?php
-    if (have_posts()) :
-      while (have_posts()) : the_post();
-        the_content();
-      endwhile;
-    endif;
-    ?>
+    <div class="wrapper lg">
+
+      <div class="contact-card">
+
+        <div class="content-wrap col-md-6">
+        <?php
+        if (have_posts()) :
+        while (have_posts()) : the_post();
+            the_content();
+        endwhile;
+        endif;
+        ?>
+        </div>
+
+        <div class="contact-form-wrap col-md-5">
+          <?php echo do_shortcode('[contact-form-7 id="e310d2b" title="Contact Form"]'); ?>
+        </div>
+
+      </div>
     </div>
   </div>
 
